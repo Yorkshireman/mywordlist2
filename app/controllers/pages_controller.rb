566f85c81344b0b02eb095f52328fcdb5d9a9210
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-29 21:38:19 +0100

    make api response parser

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,7 @@ class PagesController < ApplicationController
     word = params["word"]
     client = MWDictionaryAPI::Client.new('2a4dee74-e16f-4805-be47-0e41d7e8d840', api_type: "learners")
     result = client.search(word)
-    puts result.to_hash.keys
-    puts result.to_hash[:entries]
+    puts result.to_hash
     redirect_to root_path
   end
 end
