93b43b15eb55804580ce7dfda60c725f628eca10
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-30 12:55:10 +0100

    write test for parse_response method and make it pass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,35 @@ class PagesController < ApplicationController
     word = params["word"]
     client = MWDictionaryAPI::Client.new('2a4dee74-e16f-4805-be47-0e41d7e8d840', api_type: "learners")
     result = client.search(word)
-    puts result.to_hash
+    @dictionary_response = parse_response(result.to_hash)
     redirect_to root_path
+  end
+
+  def parse_response(hash)
+    entries_array = []
+    final_string = ""
+
+    hash[:entries].each do |entry|
+      entries_array << entry
+    end
+
+    entries_array.each do |entry|
+      final_string << "\"#{entry[:id_attribute]}\":\n"
+      
+      definition_number = 1
+      entry[:definitions].each do |definition|
+        unless (definition[:text].split("  ")).first == (definition[:text].split("  ")).last
+          definition_array = definition[:text].split("  ")
+          definition_array.first[0] = ''
+          final_string << "#{definition_number}. #{definition_array.first} (#{definition_array.last.strip}).\n"
+        else
+          definition[:text][0] = ''
+          final_string << "#{definition_number}. #{definition[:text]}.\n"
+        end
+        definition_number += 1
+      end
+      final_string << "\n"
+    end
+    final_string
   end
 end
--- sandbox.rb
+++ sandbox.rb
@@ -1,25 +1,27 @@
 hash = {:term=>"nonsense", :entries=>[{:id_attribute=>"nonsense", :head_word=>"non*sense", :pronunciation=>"ˈnɑ:nˌsɛns", :part_of_speech=>"noun", :sound=>"nonsen01.wav", :word=>"nonsense", :id=>1, :definitions=>[{:sense_number=>"1", :text=>":words or ideas that are foolish or untrue", :verbal_illustration=>"I don't know why you believe that nonsense about certain numbers being unlucky.", :cross_reference=>[]}, {:sense_number=>"2", :text=>":behavior that is silly, annoying, or unkind    see also no-nonsense", :verbal_illustration=>"He was not in the mood to put up with any nonsense from his little brother.", :cross_reference=>[]}, {:sense_number=>"3", :text=>":language that has no meaning   often used before another noun", :verbal_illustration=>"Many of the words in the poem are nonsense.", :cross_reference=>[]}], :inflections=>[]}, {:id_attribute=>"no-nonsense", :head_word=>"no–nonsense", :pronunciation=>nil, :part_of_speech=>"adjective", :sound=>nil, :word=>"no-nonsense", :id=>1, :definitions=>[{:sense_number=>"0", :text=>":very serious about doing things in a direct and efficient way without any foolishness or nonsense", :verbal_illustration=>"I want to wear something that will make me look like a no-nonsense professional.", :cross_reference=>[]}], :inflections=>[]}], :suggestions=>[]}
+final_string = ""
+entries_array = []
 
-  entries_array = []
+hash[:entries].each do |entry|
+  entries_array << entry
+end
 
-  hash[:entries].each do |entry|
-    entries_array << entry
-  end
-
-  entries_array.each do |entry|
-    puts "\"#{entry[:id_attribute]}\":\n"
-    
-    definition_number = 1
-    entry[:definitions].each do |definition|
-      unless (definition[:text].split("  ")).first == (definition[:text].split("  ")).last
-        definition_array = definition[:text].split("  ")
-        definition_array.first[0] = ''
-        puts "#{definition_number}. #{definition_array.first} (#{definition_array.last.strip})."
-      else
-        definition[:text][0] = ''
-        puts "#{definition_number}. #{definition[:text]}."
-      end
-      definition_number += 1
+entries_array.each do |entry|
+  final_string << "\"#{entry[:id_attribute]}\":\n"
+  
+  definition_number = 1
+  entry[:definitions].each do |definition|
+    unless (definition[:text].split("  ")).first == (definition[:text].split("  ")).last
+      definition_array = definition[:text].split("  ")
+      definition_array.first[0] = ''
+      puts "#{definition_number}. #{definition_array.first} (#{definition_array.last.strip})."
+    else
+      definition[:text][0] = ''
+      puts "#{definition_number}. #{definition[:text]}."
     end
-    puts "\n"
+    definition_number += 1
   end
+  puts "\n"
+end
+
+puts final_string
